d91f1e34fbe41734db34ca361e7e966480bd1d5b
Pranaywaykole <pw7891441@gmail.com>
2026-05-07 22:14:28 +0530

    Fix CORS for Vercel previews

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ const app = express()
   fetching from Gutenberg directly.
 */
 app.use(cors({
-  origin:      process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true, /* allows cookies to be sent with requests */
+  origin: true,
+  credentials: true,
 }))
 
 /*
